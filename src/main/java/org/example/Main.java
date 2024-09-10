c56dbc2370602b7e59f68c625c6f310065760bf9
sama-hh <samahajimuradovah@gmail.com>
2024-09-10 16:32:25 +0200

    remove line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ public class Main {
         Client client1 = new Client("Anar", "John", "324732040" );
         Client client2 = new Client("John", "Doe", "524234540" );
         Client client3 = new Client("Kate", "Smith", "624733450" );
-        Accounts accounts =  new Accounts();
 
         BankService bankService = new BankService();
         bankService.openAccount(client1);
